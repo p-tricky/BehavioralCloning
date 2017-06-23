e4c94f441b35781ea8d5778e531e9c39fe5022c1
Patrick Loftus <PLoftus@Astin.Local>
2017-06-23 15:56:12 -0500

    temp

--- model.py
+++ model.py
@@ -20,15 +20,15 @@ args = parser.parse_args()
 
 model = Sequential()
 model.add(Lambda(lambda x: x/255 - .5, input_shape=(160, 320, 3)))
-#model.add(Cropping2D(cropping=((70, 50), (0, 0))))
+model.add(Cropping2D(cropping=((70, 50), (0, 0))))
 model.add(Convolution2D(24, 5, 5, subsample=(2,2), activation='relu'))
-#model.add(MaxPooling2D())
+model.add(MaxPooling2D(padding='same'))
 model.add(Convolution2D(36, 5, 5, subsample=(2,2), activation='relu'))
-#model.add(MaxPooling2D())
+model.add(MaxPooling2D(padding='same'))
 model.add(Convolution2D(48, 5, 5, subsample=(1,1), activation='relu'))
-#model.add(MaxPooling2D())
+model.add(MaxPooling2D(padding='same'))
 model.add(Convolution2D(64, 3, 3, subsample=(1,1), activation='relu'))
-#model.add(MaxPooling2D())
+model.add(MaxPooling2D(padding='same'))
 model.add(Convolution2D(64, 3, 3, subsample=(1,1), activation='relu'))
 model.add(Flatten())
 model.add(Dense(100))
@@ -37,12 +37,6 @@ model.add(Dense(10))
 model.add(Dense(1))
 
 gen = DataGen()
-"""
-for imgs, angles in gen.next_train():
-    for img, angle in zip(imgs, angles):
-        cv2.imshow('angle: {}'.format(angle), img)
-        cv2.waitKey(0)
-        """
 
 model.compile(optimizer=Adam(lr=.0001), loss='mse')
 model.fit_generator(gen.next_train(), samples_per_epoch=gen._train.shape[0], nb_epoch=2,
