fcd32e8db3e661e3eb7f012d7edcf9e4f1da8670
p-tricky <phloftus@gmail.com>
2017-06-30 23:43:24 -0500

    working

--- data_gen.py
+++ data_gen.py
@@ -1,11 +1,11 @@
 import csv
 import numpy as np
-import cv2
+import matplotlib.image as mp
 from sklearn.utils import shuffle
 from sklearn.model_selection import train_test_split
 
 class DataGen:
-    def __init__(self, log_file="./data/driving_log.csv", batch_size=32):
+    def __init__(self, log_file="./data/driving_log.csv", batch_size=16):
         self._train = []
         with open(log_file) as csvf:
             reader = csv.reader(csvf)
@@ -15,10 +15,10 @@ class DataGen:
         self._train = np.array(self._train)
         self._train, self._validation = train_test_split(self._train, test_size=.2)
         self._batch_size = batch_size
-        self._views = ['center', 'flip', 'left', 'right']
+        self._views = ['center', 'left', 'right']
 
-    def samples_per_epoch():
-        return self._train.shape[0] * len(self._views)
+    def samples_per_epoch(self):
+        return self._train.shape[0] * 4
 
     def next_train(self):
             num_samples = self._train.shape[0]
@@ -30,18 +30,24 @@ class DataGen:
                     correction = .20
                     for view in self._views:
                         if view == 'center':
-                            images = [ cv2.imread("./data/" + x) for x in self._train[start:end, 0] ]
-                        else if view == 'flip':
-                            images = [ np.fliplr(cv2.imread("./data/" + x)) for x in self._train[start:end, 0] ]
-                            angles = -angles
-                        else if view == 'left':
-                            images = [ np.fliplr(cv2.imread("./data/" + x)) for x in self._train[start:end, 1] ]
-                            angles += correction
+                            images = [ mp.imread("./data/" + x.strip()) for x in self._train[start:end, 0] ]
+                            truth = np.array(angles)
+                            train_out = shuffle(np.array(images), truth)
+                            yield train_out
+                            images = [np.fliplr(img) for img in images]
+                            truth = -truth
+                            train_out = shuffle(np.array(images), truth)
+                            yield train_out
+                        elif view == 'left':
+                            images = [ mp.imread("./data/" + x.strip()) for x in self._train[start:end, 1] ]
+                            truth = np.array(angles) + correction
+                            train_out = shuffle(np.array(images), truth)
+                            yield train_out
                         else: #view == 'right'
-                            images = [ np.fliplr(cv2.imread("./data/" + x)) for x in self._train[start:end, 2] ]
-                            angles -= correction
-                        train_out = shuffle(np.array(images), np.array(angles))
-                        yield train_out
+                            images = [ mp.imread("./data/" + x.strip()) for x in self._train[start:end, 2] ]
+                            truth = np.array(angles) - correction
+                            train_out = shuffle(np.array(images), truth)
+                            yield train_out
 
 
     def next_valid(self):
@@ -50,7 +56,7 @@ class DataGen:
                 shuffle(self._validation)
                 for start in range(0, num_samples, self._batch_size):
                     end = start + self._batch_size
-                    images = [ cv2.imread("./data/" + x) for x in self._validation[start:end, 0] ]
+                    images = [ mp.imread("./data/" + x) for x in self._validation[start:end, 0] ]
                     angles = [ float(x) for x in self._validation[start:end, 3] ]
                     valid_out = shuffle(np.array(images), np.array(angles))
                     yield valid_out
--- model.py
+++ model.py
@@ -4,7 +4,7 @@ import tensorflow as tf
 import numpy as np
 from keras.models import Sequential
 from keras.optimizers import Adam
-from keras.layers import Flatten, Dense, Lambda, Cropping2D
+from keras.layers import Flatten, Dense, Lambda, Cropping2D, Dropout
 from keras.layers.convolutional import Convolution2D
 from keras.layers.pooling import MaxPooling2D
 from data_gen import DataGen
@@ -16,20 +16,22 @@ parser.add_argument('--save_to',
         default='model.h5',
         nargs='?'
 )
+parser.add_argument('--keep_prob',
+        type=float,
+        default=.5,
+        nargs='?'
+)
 args = parser.parse_args()
 
 model = Sequential()
-model.add(Lambda(lambda x: x/255 - .5, input_shape=(160, 320, 3)))
-model.add(Cropping2D(cropping=((70, 50), (0, 0))))
+model.add(Lambda(lambda x: x/255. - .5, input_shape=(160, 320, 3)))
+model.add(Cropping2D(cropping=((70, 25), (0, 0))))
 model.add(Convolution2D(24, 5, 5, subsample=(2,2), activation='relu', border_mode='same'))
-model.add(MaxPooling2D(border_mode='same'))
 model.add(Convolution2D(36, 5, 5, subsample=(2,2), activation='relu', border_mode='same'))
-model.add(MaxPooling2D(border_mode='same'))
 model.add(Convolution2D(48, 5, 5, subsample=(1,1), activation='relu', border_mode='same'))
-model.add(MaxPooling2D(border_mode='same'))
 model.add(Convolution2D(64, 3, 3, subsample=(1,1), activation='relu', border_mode='same'))
-model.add(MaxPooling2D(border_mode='same'))
 model.add(Convolution2D(64, 3, 3, subsample=(1,1), activation='relu', border_mode='same'))
+model.add(Dropout(args.keep_prob))
 model.add(Flatten())
 model.add(Dense(100))
 model.add(Dense(50))
@@ -39,7 +41,7 @@ model.add(Dense(1))
 gen = DataGen()
 
 model.compile(optimizer=Adam(lr=.0001), loss='mse')
-model.fit_generator(gen.next_train(), samples_per_epoch=gen._train.shape[0], nb_epoch=2,
+model.fit_generator(gen.next_train(), samples_per_epoch=gen.samples_per_epoch(), nb_epoch=5,
     validation_data=gen.next_valid(), nb_val_samples=gen._validation.shape[0])
 
 model.save(args.save_to)
